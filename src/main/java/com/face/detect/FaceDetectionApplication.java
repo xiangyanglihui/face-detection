fed08583353cb797fdd7f93dfed69c95a546c23a
xiangyanglihui@gmail.com <lihui19850611>
2018-09-04 22:04:06 +0800

    初始化提交

---
+++ src/main/java/com/face/detect/FaceDetectionApplication.java
@@ -0,0 +1,12 @@
+package com.face.detect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceDetectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaceDetectionApplication.class, args);
+    }
+}
